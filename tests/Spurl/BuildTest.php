3ce0466951424be8cdd5127c48a6cf754cd9e17d
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-08-17 15:59:28 +0100

    Would also help if i wrote the test properly too...

--- tests/Spurl/BuildTest.php
+++ tests/Spurl/BuildTest.php
@@ -4,7 +4,7 @@ Class SpurlBuildTest extends \PHPUnit_Framework_TestCase
 {
   public function testBuildUrl()
   {
-    $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => ['test', 'domain'], 'domain' => 'spurl', 'tld' => ['co', 'uk'], 'Path' => ['test', 'path', 'to', 'be', 'joined']]);
+    $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => ['test', 'domain'], 'domain' => 'spurl', 'tld' => ['co', 'uk'], 'path' => ['test', 'path', 'to', 'be', 'joined']]);
     $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
   }
 }
