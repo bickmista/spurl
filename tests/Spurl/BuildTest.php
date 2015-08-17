e160b271ffffd765e5d40a3ef9dc1bcb3a8b964c
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-08-17 15:57:25 +0100

    Didnt follow function naming scheme, my bad

--- tests/Spurl/BuildTest.php
+++ tests/Spurl/BuildTest.php
@@ -2,7 +2,7 @@
 
 Class SpurlBuildTest extends \PHPUnit_Framework_TestCase
 {
-  public function buildUrlTest()
+  public function testBuildUrl()
   {
     $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => ['test', 'domain'], 'domain' => 'spurl', 'tld' => ['co', 'uk'], 'Path' => ['test', 'path', 'to', 'be', 'joined']]);
     $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
