479715ea00d078a250d53206d68471c7b677840b
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-08-17 16:06:15 +0100

    Added an additional test to check if strings also work

--- tests/Spurl/BuildTest.php
+++ tests/Spurl/BuildTest.php
@@ -2,9 +2,14 @@
 
 Class SpurlBuildTest extends \PHPUnit_Framework_TestCase
 {
-  public function testBuildUrl()
+  public function testUrlBuildArrays()
   {
     $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => ['test', 'domain'], 'domain' => 'spurl', 'tld' => ['co', 'uk'], 'path' => ['test', 'path', 'to', 'be', 'joined']]);
+    $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
+  }
+  public function testUrlBuildStrings()
+  {
+    $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => 'test.domain', 'domain' => 'spurl', 'tld' => 'co.uk', 'path' => 'test/path/to/be/joined']);
     $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
   }
 }
