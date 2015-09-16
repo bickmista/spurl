27bc9927e90866105737368e49592d17537c44e9
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-16 16:18:57 +0100

    Updated build function (was outdated)

--- tests/Spurl/BuildTest.php
+++ tests/Spurl/BuildTest.php
@@ -4,12 +4,12 @@ Class SpurlBuildTest extends \PHPUnit_Framework_TestCase
 {
   public function testUrlBuildArrays()
   {
-    $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => ['test', 'domain'], 'domain' => 'spurl', 'tld' => ['co', 'uk'], 'path' => ['test', 'path', 'to', 'be', 'joined']]);
+    $url = Spurl\Url::build(['protocol' => 'https', 'host' => ['test', 'domain', 'co.uk'], 'path' => ['test', 'path', 'to', 'be', 'joined']]);
     $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
   }
   public function testUrlBuildStrings()
   {
-    $url = Spurl\Url::build(['protocol' => 'https', 'subdomain' => 'test.domain', 'domain' => 'spurl', 'tld' => 'co.uk', 'path' => 'test/path/to/be/joined']);
+    $url = Spurl\Url::build(['protocol' => 'https', 'host' => ['test.domain', 'spurl', 'co.uk'], 'path' => 'test/path/to/be/joined']);
     $this->assertEquals($url, 'https://test.domain.spurl.co.uk/test/path/to/be/joined/');
   }
 }
